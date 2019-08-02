2d94a04e967ab85c03d651ac78abb81bb193ff3f
xutongbao <1183391880@qq.com>
2019-08-02 14:02:18 +0800

    todolist

---
+++ test/src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      name: 'xu'
+    }
+  }
+
+  handleAdd() {
+    let {count} = this.state
+    count = count + 1
+
+    //错误的写法
+    //this.state.count = this.state.count + 1 
+
+    this.setState({
+      count
+    })
+  }
+
+  render() {
+    let {count} = this.state
+    return (
+      <div>
+        {count}
+        <div>
+          <button onClick={this.handleAdd.bind(this)}>加</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
