d1d32f6b0710da051c5cdfa28ebe6568605c4ec2
xutongbao <1183391880@qq.com>
2019-08-05 08:14:25 +0800

    todolist数据持久化

--- m-app/src/pages/Index.js
+++ m-app/src/pages/Index.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
-import List from '../components/List.js'
+import React, {Component} from 'react'
+import List from '../components/List'
 
 class Index extends Component {
   constructor(props) {
     super(props)
     this.state = {
       text: '',
-      list: [],
+      list: []
     }
   }
 
@@ -16,28 +16,24 @@ class Index extends Component {
     })
   }
 
-  handleInputEnter(e) {
+  handleInputKeyDown(e) {
+    let {text,list} = this.state
     if (e.keyCode === 13) {
-      let {
-        text,
-        list,
-      } = this.state
-
       list.push({
         id: (new Date()).getTime(),
         text,
         checked: false,
       })
       this.setState({
-        list,
         text: '',
+        list,
       })
-    }
+    } 
   }
 
   handleUpdateList(id, checked) {
-    let { list } = this.state
-    for (let i = 0; i < list.length; i ++) {
+    let {list} = this.state
+    for (let i = 0; i < list.length; i++) {
       if (list[i].id === id) {
         list[i].checked = checked
       }
@@ -47,6 +43,26 @@ class Index extends Component {
     })
   }
 
+  handleDelete(id) {
+    let {list} = this.state
+    list = list.filter(item => item.id !== id)
+    this.setState({
+      list
+    })
+  }
+
+  componentDidMount() {
+    let list = JSON.parse(localStorage.getItem('list')) ? JSON.parse(localStorage.getItem('list')) : []
+    this.setState({
+      list
+    })
+  }
+
+  componentDidUpdate() {
+    let {list} = this.state
+    localStorage.setItem('list', JSON.stringify(list))
+  }
+
   render() {
     let {
       text,
@@ -54,21 +70,13 @@ class Index extends Component {
     } = this.state
     return (
       <div>
-        <div>
-          <input type='text' value={text} onKeyDown={this.handleInputEnter.bind(this)} onChange={this.handleInput.bind(this, 'text')}></input>
-        </div>
-        <div>
-          全部({list.length})
-          <List list={list} checked="all" onUpdateList={this.handleUpdateList.bind(this)}></List>
-        </div>
-        <div>
-          正在进行({list.filter(item => item.checked === false).length}):
-        </div>
-        <List list={list} checked={false} onUpdateList={this.handleUpdateList.bind(this)}></List>
-        <div>
-          已经完成({list.filter(item => item.checked).length}):
-        </div>
-        <List list={list} checked={true} onUpdateList={this.handleUpdateList.bind(this)}></List>
+        <input type="text" value={text} onChange={this.handleInput.bind(this, 'text')} onKeyDown={this.handleInputKeyDown.bind(this)}></input>
+        <div>全部数据({list.length})：</div>
+        <List list={list} checked="all" onUpdateList={this.handleUpdateList.bind(this)} onDelete={this.handleDelete.bind(this)}></List>
+        <div>正在进行({list.filter(item => item.checked === false).length})：</div>
+        <List list={list} checked={false} onUpdateList={this.handleUpdateList.bind(this)} onDelete={this.handleDelete.bind(this)}></List>
+        <div>已经完成({list.filter(item => item.checked).length})：</div>
+        <List list={list} checked={true} onUpdateList={this.handleUpdateList.bind(this)} onDelete={this.handleDelete.bind(this)}></List>
       </div>
     )
   }
